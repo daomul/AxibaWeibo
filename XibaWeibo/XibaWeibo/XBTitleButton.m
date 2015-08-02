30c71dfde0a43a6161496f929d70e3063b6525d5
K3CLOUDBOS <K3CloudBOS@scmdeiMac.local>
2015-08-02 16:01:18 +0800

    如果想在系统设置完控件的尺寸后，再做修改，而且要保证修改成功，一般都是在setFrame:中设置

--- XibaWeibo/XibaWeibo/XBTitleButton.m
+++ XibaWeibo/XibaWeibo/XBTitleButton.m
@@ -41,6 +41,17 @@
     self.imageView.x = CGRectGetMaxX(self.titleLabel.frame);
 }
 
+// 目的：想在系统计算和设置完按钮的尺寸后，再修改一下尺寸
+/**
+ *  重写setFrame:方法的目的：拦截设置按钮尺寸的过程
+ *  如果想在系统设置完控件的尺寸后，再做修改，而且要保证修改成功，一般都是在setFrame:中设置
+ */
+- (void)setFrame:(CGRect)frame
+{
+    //frame.size.width += HWMargin;
+    [super setFrame:frame];
+}
+
 #pragma  mark --  设置UIButton的时候自适应尺寸
 -(void)setTitle:(NSString *)title forState:(UIControlState)state
 {

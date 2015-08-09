a27720589a8a1e0eb652754c6937c2bd650091a9
K3CLOUDBOS <K3CloudBOS@scmdeiMac.local>
2015-08-09 11:11:42 +0800

    __block的变量问题

--- XibaWeibo/XibaWeibo/AppDelegate.m
+++ XibaWeibo/XibaWeibo/AppDelegate.m
@@ -74,7 +74,7 @@
      *  4.后台运行状态
      */
     // 向操作系统申请后台运行的资格，能维持多久，是不确定的
-    UIBackgroundTaskIdentifier task = [application beginBackgroundTaskWithExpirationHandler:^{
+    __block UIBackgroundTaskIdentifier task = [application beginBackgroundTaskWithExpirationHandler:^{
          // 当申请的后台运行时间已经结束（过期），就会调用这个block
         
         // 过期则需要结束任务
